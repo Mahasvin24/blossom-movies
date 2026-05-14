3fbac0d013e4b7019b0130852c1d87b9895cc6e1
Mahasvin Shanmugapriya Manikandan <mahasvin.manikandan@gmail.com>
2026-05-13 23:22:41 -0700

    Initial Commit

---
+++ blossom-movies/blossom_moviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  blossom_moviesApp.swift
+//  blossom-movies
+//
+//  Created by Mahasvin Shanmugapriya Manikandan on 5/13/26.
+//
+
+import SwiftUI
+
+@main
+struct blossom_moviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
