f60ff83da433c8939684b830d63ad6ebfebb78b5
Mahasvin Shanmugapriya Manikandan <mahasvin.manikandan@gmail.com>
2026-06-19 12:16:01 -0700

    network error protocol

--- blossom-movies/Constants.swift
+++ blossom-movies/Constants.swift
@@ -17,7 +17,7 @@ struct Constants {
     static let playString = "Play"
     static let trendingMovesString = "Trending Movies"
     static let trendingTVString = "Trending TV"
-    static let topRatedMovieString = "Top Rate Movies"
+    static let topRatedMovieString = "Top Rated Movies"
     static let topRatedTVString = "Top Rated TV"
     
     // Icons
@@ -26,9 +26,17 @@ struct Constants {
     static let searchIconString = "magnifyingglass"
     static let downloadsIconString = "arrow.down.to.line"
     
+    // Test Titles
     static let testTitleURL = "https://image.tmdb.org/t/p/w500/nnl6OWkyPpuMm595hmAxNW3rZFn.jpg"
     static let testTitleURL2 = "https://image.tmdb.org/t/p/w500/d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg"
     static let testTitleURL3 = "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg"
+    
+    struct Icons {
+        static let homeIconString = "house"
+        static let upcomingIconString = "play.circle"
+        static let searchIconString = "magnifyingglass"
+        static let downloadsIconString = "arrow.down.to.line"
+    }
 }
 
 extension Text {
---
+++ blossom-movies/DataFetcher.swift
@@ -0,0 +1,11 @@
+//
+//  DataFetcher.swift
+//  blossom-movies
+//
+//  Created by Mahasvin Shanmugapriya Manikandan on 6/19/26.
+//
+
+import Foundation
+
+let tmdbBaseURL = APIConfig.shared?.tmdbBaseURL
+let tmdbAPIKey = APIConfig.shared?.tmdbAPIKey
