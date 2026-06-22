8b7616cf8a8611e981bde1d1b28b22bf2307a039
Mahasvin Shanmugapriya Manikandan <mahasvin.manikandan@gmail.com>
2026-06-21 20:47:42 -0700

    view model for title fetch

---
+++ blossom-movies/ViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  ViewModel.swift
+//  blossom-movies
+//
+//  Created by Mahasvin Shanmugapriya Manikandan on 6/21/26.
+//
+
+import Foundation
+
+@Observable
+class ViewModel {
+    enum FetchStatus {
+        case notStarted
+        case fetching
+        case success
+        case failed(underlyingError: Error)
+    }
+    
+    private(set) var homeStatus: FetchStatus = .notStarted
+    private let dataFetcher = DataFetcher()
+    var trendingMovies: [Title] = []
+    
+    func getTitles() async {
+        homeStatus = .fetching
+        
+        do {
+            trendingMovies = try await dataFetcher.fetchTitles(for: "movie")
+            homeStatus = .success
+        } catch {
+            print(error)
+            homeStatus = .failed(underlyingError: error)
+        }
+        
+        return
+    }
+}
