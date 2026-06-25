60aaea7de2473c5a0545e09b4a2c459a63785f89
Mahasvin Shanmugapriya Manikandan <mahasvin.manikandan@gmail.com>
2026-06-24 21:32:17 -0700

    navigation view

--- blossom-movies/HomeView.swift
+++ blossom-movies/HomeView.swift
@@ -9,65 +9,82 @@ import SwiftUI
 
 struct HomeView: View {
     let viewModel = ViewModel()
+    @State private var titleDetailPath = NavigationPath()
     
     var body: some View {
-        GeometryReader { geo in
-            ScrollView(.vertical) {
-                // view model
-                switch viewModel.homeStatus {
-                case .notStarted:
-                    EmptyView()
-                case .fetching:
-                    ProgressView()
-                        .frame(width: geo.size.width, height: geo.size.height)
-                case .success:
-                    LazyVStack {
-                        // Opening image
-                        AsyncImage(url: URL(string: viewModel.heroTitle.posterPath ?? "")) { image in
-                            image
-                                .resizable()
-                                .scaledToFit()
-                                .overlay {
-                                    LinearGradient(
-                                        stops: [
-                                            Gradient.Stop(color: .clear, location: 0.8),
-                                            Gradient.Stop(color: .gradient, location: 1)
-                                        ],
-                                        startPoint: .top,
-                                        endPoint: .bottom)
-                                }
-                        } placeholder: {
-                            ProgressView()
-                        }
-                        .frame(width: geo.size.width, height: geo.size.height * 0.85)
-                        
-                        // Home page buttons
-                        HStack {
-                            Button {
-                            } label: {
-                                Text(Constants.playString)
-                                    .ghostButton()
+        NavigationStack(path: $titleDetailPath) {
+            GeometryReader { geo in
+                ScrollView(.vertical) {
+                    // view model
+                    switch viewModel.homeStatus {
+                    case .notStarted:
+                        EmptyView()
+                    case .fetching:
+                        ProgressView()
+                            .frame(width: geo.size.width, height: geo.size.height)
+                    case .success:
+                        LazyVStack {
+                            // Opening image
+                            AsyncImage(url: URL(string: viewModel.heroTitle.posterPath ?? "")) { image in
+                                image
+                                    .resizable()
+                                    .scaledToFit()
+                                    .overlay {
+                                        LinearGradient(
+                                            stops: [
+                                                Gradient.Stop(color: .clear, location: 0.8),
+                                                Gradient.Stop(color: .gradient, location: 1)
+                                            ],
+                                            startPoint: .top,
+                                            endPoint: .bottom)
+                                    }
+                                    .overlay {
+                                        LinearGradient(
+                                            stops: [
+                                                Gradient.Stop(color: .gradient, location: 0),
+                                                Gradient.Stop(color: .clear, location: 0.2)
+                                            ],
+                                            startPoint: .top,
+                                            endPoint: .bottom
+                                        )
+                                    }
+                            } placeholder: {
+                                ProgressView()
                             }
+                            .frame(width: geo.size.width, height: geo.size.height * 0.85)
                             
-                            Button {
-                            } label: {
-                                Text(Constants.downloadString)
-                                    .ghostButton()
+                            // Home page buttons
+                            HStack {
+                                Button {
+                                    titleDetailPath.append(viewModel.heroTitle)
+                                } label: {
+                                    Text(Constants.playString)
+                                        .ghostButton()
+                                }
+                                
+                                Button {
+                                } label: {
+                                    Text(Constants.downloadString)
+                                        .ghostButton()
+                                }
                             }
+                             
+                            // Scrollable
+                            HorizontalListView(header: Constants.trendingMovesString, titles: viewModel.trendingMovies)
+                            HorizontalListView(header: Constants.trendingTVString, titles: viewModel.trendingTV)
+                            HorizontalListView(header: Constants.topRatedMovieString, titles: viewModel.topRatedMovies)
+                            HorizontalListView(header: Constants.topRatedTVString, titles: viewModel.topRatedTV)
                         }
-                        
-                        // Scrollable
-                        HorizontalListView(header: Constants.trendingMovesString, titles: viewModel.trendingMovies)
-                        HorizontalListView(header: Constants.trendingTVString, titles: viewModel.trendingTV)
-                        HorizontalListView(header: Constants.topRatedMovieString, titles: viewModel.topRatedMovies)
-                        HorizontalListView(header: Constants.topRatedTVString, titles: viewModel.topRatedTV)
+                        .navigationDestination(for: Title.self) { title in
+                            TitleDetailView(title: title)
+                        }
+                    case .failed(let error):
+                        Text("Error: \(error.localizedDescription)")
                     }
-                case .failed(let error):
-                    Text("Error: \(error.localizedDescription)")
                 }
-            }
-            .task {
-                await viewModel.getTitles()
+                .task {
+                    await viewModel.getTitles()
+                }
             }
         }
     }
